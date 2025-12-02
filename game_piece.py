ea1585eac6a18e938c784b1e87856af17540c9b7
Valentin Lindorfer <valentin.lindorfer@spyteam.one>
2025-12-02 21:48:55 +0100

    added all chess Pieces to game_piece.py; Enums might be changed or removed in the future

--- game_piece.py
+++ game_piece.py
@@ -5,8 +5,8 @@ class Piece:
     x = 0
     y = 0
 
-    def __init__(self, id, x, y):
-        self.id = id
+    def __init__(self, piece_id, x, y):
+        self.id = piece_id
         self.x = x
         self.y = y
 
@@ -24,4 +24,65 @@ class Pawn(Piece):
     def move(self, x, y):
         pass
 
-    def promote(self, x, ):
+    def en_passant(self, x, y):
+        pass
+
+    def promote(self, x): # todo might pass new piece directly
+        pass
+
+class Knight(Piece):
+    def move(self, x, y):
+        pass
+
+
+class Bishop(Piece):
+    class Moves(Enum):
+        DIAGONAL_LEFT_UP = 0
+        DIAGONAL_LEFT_DOWN = 1
+        DIAGONAL_RIGHT_UP = 2
+        DIAGONAL_RIGHT_DOWN = 3
+
+    def move(self, x, y):
+        pass
+
+class Rook(Piece):
+    class Moves(Enum):
+        UP = 0
+        DOWN = 1
+        LEFT = 2
+        RIGHT = 3
+
+    def move(self, x, y):
+        pass
+
+    def castle(self, x, y):
+        pass
+
+class Queen(Piece):
+    class Moves(Enum):
+        UP = 0
+        DOWN = 1
+        LEFT = 2
+        RIGHT = 3
+        DIAGONAL_LEFT_UP = 4
+        DIAGONAL_LEFT_DOWN = 5
+        DIAGONAL_RIGHT_UP = 6
+        DIAGONAL_RIGHT_DOWN = 7
+
+    def move(self, x, y):
+        pass
+
+
+class King(Piece):
+    class Moves(Enum):
+        UP = 0
+        DOWN = 1
+        LEFT = 2
+        RIGHT = 3
+        DIAGONAL_LEFT_UP = 4
+        DIAGONAL_LEFT_DOWN = 5
+        DIAGONAL_RIGHT_UP = 6
+        DIAGONAL_RIGHT_DOWN = 7
+
+    def move(self, x, y):
+        pass
