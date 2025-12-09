89fdb035457597b2841d46399cab230f92814f6d
Valentin Lindorfer <valentin.lindorfer@spyteam.one>
2025-12-09 07:08:59 +0100

    fixed pawn actually being able to capture their own pieces, added some methods to king piece

--- src/game_piece.py
+++ src/game_piece.py
@@ -15,8 +15,12 @@ class Piece:
         else:
             raise Exception("Invalid Move!")
 
-    def check_validity(self, x:int, y:int):
+    def check_validity(self, x:int, y:int) -> bool:
         return True
+
+    def puts_king_in_check(self, x:int, y:int) -> bool:
+        pass # todo actually check state of game/state
+        return False
 
 class Pawn(Piece):
     is_en_passant_able:bool = False
@@ -38,7 +42,7 @@ class Pawn(Piece):
         else:
             raise Exception("Invalid Move!")
 
-    def check_validity(self, x:int, y:int):
+    def check_validity(self, x:int, y:int) -> bool:
         """
             if out of range of moves
                 if pawn at square 2
@@ -51,31 +55,34 @@ class Pawn(Piece):
         if not self.is_valid_coordinate(x, y):
             print("Invalid Coordinate")
             return False
+
         elif not self.can_capture(x, y):
-            if self.puts_king_in_check(x, y):
-                print("Puts King in Check")
-                return False
-            elif self.path_blocked(x, y):
+            if self.path_blocked(x, y):
                 print("Path is Blocked")
                 return False
 
+        if self.puts_king_in_check(x, y):
+            print("Puts King in Check")
+            return False
+
         return True
 
-    def is_valid_coordinate(self, x:int, y:int):
+
+    def is_valid_coordinate(self, x:int, y:int) -> bool:
         if x == self.x and y == self.y:  # todo might get changed; depending on if splitting allows this
             return False  # Cannot put piece where piece stood
 
         if self.side:
-            if y <= self.y:  # todo might get changed; depending on if splitting allows this
+            if y <= self.y:
                 return False
         else:
-            if y >= self.y:  # todo might get changed; depending on if splitting allows this
+            if y >= self.y:
                 return False
 
         if (x < 1 or x > 8) or (y < 1 or y > 8):
             return False # Out of bounds
 
-        elif not (abs(y - self.y) == 1 or (abs(y - self.y) == 2 and self.y == (2 if self.side else 7))): # todo might be wrong
+        elif not (abs(y - self.y) == 1 or (abs(y - self.y) == 2 and self.y == (2 if self.side else 7))):
             return False
 
         elif abs(self.x - x) > 1:
@@ -87,11 +94,8 @@ class Pawn(Piece):
 
         return True
 
-    def puts_king_in_check(self, x:int, y:int):
-        pass # todo actually check state of game/state
-        return False
 
-    def path_blocked(self, x:int, y:int):
+    def path_blocked(self, x:int, y:int) -> bool:
         """path:list = [
             Game.board[self.y + 1][self.x],
             Game.board[self.y + 2][self.x]
@@ -108,8 +112,8 @@ class Pawn(Piece):
 
         return False
 
-    def can_capture(self, x:int, y:int):
-        if issubclass(type(self.board[y - 1][x - 1]), Piece) and abs(self.x - x) == 1:
+    def can_capture(self, x:int, y:int) -> bool:
+        if issubclass(type(self.board[y - 1][x - 1]), Piece) and self.board[y - 1][x - 1].side != self.side and abs(self.x - x) == 1:
             return True
 
         elif type(self.board[(y - 2) if self.side else (y + 1)][x - 1]) == Pawn and self.board[(y - 2) if self.side else (y + 1)][x - 1].is_en_passant_able:
@@ -138,5 +142,30 @@ class Queen(Piece):
         pass
 
 class King(Piece):
-    def check_validity(self, x:int, y:int) -> bool:
+    def check_validity(self, x: int, y: int) -> bool:
+        """
+        """
+        if not self.is_valid_coordinate(x, y):
+            print("Invalid Coordinate")
+            return False
+
+        if self.puts_king_in_check(x, y):
+            print("Puts King in Check")
+            return False
+
+        return True
+
+    def is_valid_coordinate(self, x: int, y: int):
+        if x == self.x and y == self.y:
+            return False
+
+        elif (x < 1 or x > 8) or (y < 1 or y > 8):
+            return False  # Out of bounds
+
+        elif (abs(self.x - x) > 1 or abs(self.y - y) > 1) and (not self.can_castle(x, y)): # todo might be changed
+            return False
+
+        return True
+
+    def can_castle(self, x, y):
         pass
